b88fa4a06d03272b89aa3df6ab8936a346ac544a
Manuel De la Penna <manueldelapenna@gmail.com>
2021-09-17 15:05:31 -0300

    version asincrónica

--- src/first-locatable.ts
+++ src/first-locatable.ts
@@ -1,7 +1,29 @@
 //var locatePath = require("locate-path");
 
-const fs = require('fs-extra');
+//const fs = require('fs');
+//export const firstLocatable: FirstLocatable = async (pathList:string[])=>pathList.find((path)=>fs.existsSync(path));
+
+const fs = require('fs').promises;
 
 type FirstLocatable = (pathList:string[]) => Promise<string|undefined>
 
-export const firstLocatable: FirstLocatable = async (pathList:string[])=>pathList.find((path)=>fs.existsSync(path));
+const asyncSome = async (arr:string[], predicate:(element:string)=>Promise<string|undefined>) => {
+	for(let e of arr) {
+		if (await predicate(e)) return e;
+	}
+	return undefined;
+};
+
+export const firstLocatable: FirstLocatable = async (pathList:string[])=>
+    await asyncSome(pathList, async (path) => {
+        try{
+            return await fs.stat(path)
+        }catch(err){
+            //@ts-ignore
+            if(err.code = 'ENOENT'){
+                return undefined
+            }
+            throw err
+        }
+    });
+    
